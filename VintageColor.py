42dfd9cfc8519f1d152777d6161824440953881d
Arthur Carabott <arthurc@gmail.com>
2018-06-02 14:08:39 +0100

    use default theme for command

--- VintageColor.py
+++ VintageColor.py
@@ -1,10 +1,14 @@
 import sublime
 import sublime_plugin
 
-settings = sublime.load_settings('VintageColor.sublime-settings')
-
 
 class VintageColor(sublime_plugin.EventListener):
+    def __init__(self):
+        self.default_theme = "";
+
+    def on_new(self, view):
+        self.default_theme = view.settings().get('color_scheme')
+
     def on_post_text_command(self, view, command_name, args):
         mode_commands = [
             'enter_insert_mode',
@@ -17,27 +21,38 @@ class VintageColor(sublime_plugin.EventListener):
         if command_name not in mode_commands:
             return
 
-        is_insert_mode = command_name == 'enter_insert_mode'
-        is_command_mode = (command_name == 'exit_insert_mode' or
-                           command_name == 'exit_visual_mode')
-        is_visual_mode = command_name == 'enter_visual_mode'
-        is_visual_line_mode = command_name == 'enter_visual_line_mode'
+        mode = ('insert' if command_name == 'enter_insert_mode' else
+                'command' if (command_name == 'exit_insert_mode' or
+                              command_name == 'exit_visual_mode') else
+                'visual' if command_name == 'enter_visual_mode' else
+                'visual_line' if command_name == 'enter_visual_line_mode' else
+                None)
 
-        mode = ('insert' if is_insert_mode else
-                'command' if is_command_mode else
-                'visual' if is_visual_mode else
-                'visual_line' if is_visual_line_mode else None)
+        if command_name == 'enter_insert_mode':
+            mode = 'insert'
+        elif command_name in ['exit_insert_mode', 'exit_visual_mode']:
+            mode = 'command'
+        elif command_name == 'enter_visual_mode':
+            mode = 'visual'
+        elif command_name == 'enter_visual_line_mode':
+            mode = 'visual_line'
 
-        if mode is None:
-            return
 
-        theme = settings.get('{}_theme'.format(mode))
+        settings = sublime.load_settings('VintageColor.sublime-settings')
+
+        # update color scheme
+        if mode == 'command':
+            theme = self.default_theme
+        else:
+            theme = settings.get('{}_theme'.format(mode))
+
         view.settings().set('color_scheme', theme)
 
+        # highlight line
         highlight_line = settings.get("highlight_line")
 
-        if mode == 'insert' or mode == 'command':
-            settings_highlight = settings.get('{}_highlight_line'.format(mode))
+        if mode == 'insert' :
+            settings_highlight = settings.get('insert_highlight_line')
             highlight_line = highlight_line or settings_highlight
 
         view.settings().set("highlight_line", highlight_line)
