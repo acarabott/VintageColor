a9a3bd2b84d69bea2822669b38a0e4dbcae28afd
Arthur Carabott <arthurc@gmail.com>
2018-06-02 16:07:25 +0100

    fix settings loading

--- VintageColor.py
+++ VintageColor.py
@@ -3,12 +3,6 @@ import sublime_plugin
 
 
 class VintageColor(sublime_plugin.EventListener):
-    def __init__(self):
-        self.default_theme = "";
-
-    def on_new(self, view):
-        self.default_theme = view.settings().get('color_scheme')
-
     def on_post_text_command(self, view, command_name, args):
         mode_commands = [
             'enter_insert_mode',
@@ -37,22 +31,22 @@ class VintageColor(sublime_plugin.EventListener):
         elif command_name == 'enter_visual_line_mode':
             mode = 'visual_line'
 
-
-        settings = sublime.load_settings('VintageColor.sublime-settings')
+        plugin_settings = sublime.load_settings('VintageColor.sublime-settings')
+        global_settings = sublime.load_settings("Preferences.sublime-settings")
 
         # update color scheme
         if mode == 'command':
-            theme = self.default_theme
+            theme = global_settings.get("color_scheme")
         else:
-            theme = settings.get('{}_theme'.format(mode))
+            theme = plugin_settings.get('{}_theme'.format(mode))
 
         view.settings().set('color_scheme', theme)
 
         # highlight line
-        highlight_line = settings.get("highlight_line")
+        highlight_line = global_settings.get("highlight_line")
 
         if mode == 'insert' :
-            settings_highlight = settings.get('insert_highlight_line')
+            settings_highlight = plugin_settings.get('insert_highlight_line')
             highlight_line = highlight_line or settings_highlight
 
         view.settings().set("highlight_line", highlight_line)
