adb19cdb367a9804f521f0e9f78cc41acffef5c6
Arthur Carabott <arthurc@gmail.com>
2018-06-02 17:21:45 +0100

    improve heuristic for detecting insert mode

--- VintageColor.py
+++ VintageColor.py
@@ -3,24 +3,23 @@ import sublime_plugin
 
 
 class VintageColor(sublime_plugin.EventListener):
+    def __init__(self):
+        self.current_mode = 'command'
+
     def on_post_text_command(self, view, command_name, args):
-        mode_commands = [
-            'enter_insert_mode',
-            'exit_insert_mode',
-            'enter_visual_mode',
-            'enter_visual_line_mode',
-            'exit_visual_mode'
-        ]
+        is_insert_mode = not view.settings().get('inverse_caret_state')
 
-        if command_name not in mode_commands:
-            return
-
-        mode = ('insert' if command_name == 'enter_insert_mode' else
+        mode = ('insert' if is_insert_mode else
                 'command' if (command_name == 'exit_insert_mode' or
                               command_name == 'exit_visual_mode') else
                 'visual' if command_name == 'enter_visual_mode' else
                 'visual_line' if command_name == 'enter_visual_line_mode' else
                 None)
+
+        if mode is None or mode == self.current_mode:
+            return
+
+        self.current_mode = mode
 
         if command_name == 'enter_insert_mode':
             mode = 'insert'
